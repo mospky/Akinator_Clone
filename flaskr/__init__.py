99aec37074a2f00e84fd0d7d6588fdaf7d5c3097
mospky <kameyoc17@icloud.com>
2023-02-11 22:00:18 +0900

    pip unistall flask-bootstrap

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,8 +2,6 @@ import os
 
 from flask import Flask
 
-from flask_bootstrap import Bootstrap
-
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
@@ -11,8 +9,6 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
-
-    bootstrap = Bootstrap(app)
 
     #DEBUG
     str = "DEBUG : " + "__name__ = " + __name__
